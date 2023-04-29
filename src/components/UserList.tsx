50d48ae98e8b320d198375f2f939ea39dc8087ed
volkovich <volkoviysash@gmail.com>
2023-04-29 20:39:23 +0300

    feat: Create UseActions hook.

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -1,10 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
-import { useTypedSelector } from "./hooks/useTypedSelector";
+import { useTypedSelector } from "../hooks/useTypedSelector";
+import { useDispatch } from "react-redux";
+import { fetchUsers } from "../store/action-creators/user";
+import type {} from "redux-thunk/extend-redux";
+import { useActions } from "../hooks/useAction";
 
 const UserList: React.FC = () => {
   const { users, error, loading } = useTypedSelector((state) => state.user);
-  return <div></div>;
+  const { fetchUsers } = useActions();
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  if (loading) {
+    return <h1>Is loading...</h1>;
+  }
+  if (error) {
+    return <h1>{error}</h1>;
+  }
+
+  return (
+    <div>
+      {users.map((user) => (
+        <div key={user.id}>{user.name}</div>
+      ))}
+    </div>
+  );
 };
 
 export default UserList;
--- src/store/action-creators/user.ts
+++ src/store/action-creators/user.ts
@@ -9,10 +9,12 @@ export const fetchUsers = () => {
       const response = await axios.get(
         "https://jsonplaceholder.typicode.com/users"
       );
-      dispatch({
-        type: UserActionTypes.FETCH_USERS_SUCCESS,
-        payload: response.data,
-      });
+      setTimeout(() => {
+        dispatch({
+          type: UserActionTypes.FETCH_USERS_SUCCESS,
+          payload: response.data,
+        });
+      }, 1500);
     } catch (e) {
       dispatch({
         type: UserActionTypes.FETCH_USERS_ERROR,
